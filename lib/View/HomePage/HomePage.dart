3ddaf5e442bc43b6ba281fe410e6a025fa0ec61d
AGhafurr <aghafurabdul@gmail.com>
2023-12-01 01:25:15 +0700

    Success

--- lib/View/HomePage/HomePage.dart
+++ lib/View/HomePage/HomePage.dart
@@ -71,7 +71,7 @@ class _HomePageState extends State<HomePage> {
             ),
             onPressed: () {
               Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return profill();
+                return ProfilePage();
               }));
             },
           ),
